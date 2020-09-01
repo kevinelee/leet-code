fc1e66da7eb7e64bc4fc424992e9a9d0aee7b2bd
Kevin E. Lee <kevin.ej.lee@gmail.com>
2020-09-01 16:31:00 -0700

    interview cake added

---
+++ interview-cake-inflight.js
@@ -0,0 +1,17 @@
+function canTwoMoviesFillFlight(movieLengths, flightLength) {
+
+  const movieLengthsSeen = new Set();
+
+  for (let i = 0; i < movieLengths.length; i++) {
+    const firstMovieLength = movieLengths[i];
+
+    const matchingSecondMovieLength = flightLength - firstMovieLength;
+    if (movieLengthsSeen.has(matchingSecondMovieLength)) {
+      return true;
+    }
+
+    movieLengthsSeen.add(firstMovieLength);
+  }
+
+  return false;
+}
